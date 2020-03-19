11f4e7184eb0de68d12d8579dae1238dc78e159c
Renan Bezerra <renanbg90@gmail.com>
2020-03-18 22:27:12 -0300

    JUnit-1.1

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -15,7 +16,7 @@ public class LocacaoService {
 		locacao.setFilme(filme);
 		locacao.setUsuario(usuario);
 		locacao.setDataLocacao(new Date());
-		locacao.setValor(filme.getPrecoLocacao());
+		locacao.setValor(filme.getPrecoLocacao()); 
 
 		//Entrega no dia seguinte
 		Date dataEntrega = new Date();
@@ -30,5 +31,19 @@ public class LocacaoService {
 
 	public static void main(String[] args) {
 		
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme",2,5.0);
+		
+		//ação
+			Locacao locacao = service.alugarFilme(usuario, filme);
+			
+		//verificação
+			System.out.println(locacao.getValor()== 5.0);
+			System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(),new Date()));
+			System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(),DataUtils.obterDataComDiferencaDias(1)));
+			
+			
 	}
 }
